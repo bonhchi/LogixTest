807f03c243a4b201e45cdfc1998a1bc2fdf9dcc3
Admin <Admin@DESKTOP-EH1DB4N>
2022-11-28 20:50:59 +0700

    Add project files.

---
+++ LogixTest.Domain/Domain/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogixTest.Domain.Domain
+{
+    internal class Movie
+    {
+    }
+}
